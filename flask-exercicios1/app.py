d7871b16b003a5a8e886309819f899bff60f57ff
gustamiranda00 <gustavomiranda@colegiopaulodetarso.com.br>
2025-11-12 14:43:24 +0000

    git hub

--- flask-exercicios1/app.py
+++ flask-exercicios1/app.py
@@ -5,22 +5,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Hello, Flask !!</h1>"
+
 # Exercício 2
 @app.route('/versao')
 def versao():
     versao = "1.0.0"
     return f"App v{versao}"
+
 # Exercício 3 
 @app.route('/saudar/<nome>')
 def saudar(nome):
     nome = nome.capitalize()
     return f"Olá, {nome}!"
+
 # Exercício 4 
 @app.route('/quadrado/<int:n>')
 def quadrado(n):
     resultado = n ** 2
     return f"{n}² = {resultado}"
+
 # Exercício 5 
 @app.route('/home')
 def home():
-    return redirect(url_for('index'))
+    return redirect(url_for('index')) 
+
+# Exercício 6 - Página HTML via template
+@app.route('/pagina')
+def pagina():
+    return render_template('pagina.html')
+
