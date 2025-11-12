0354cd6b7611beaf93a62b81085fa1f482e5c595
gustamiranda00 <gustavomiranda@colegiopaulodetarso.com.br>
2025-11-12 14:39:45 +0000

    git hub

--- flask-exercicios1/app.py
+++ flask-exercicios1/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-
+# Exercício 1
 @app.route('/')
 def index():
     return "<h1>Hello, Flask !!</h1>"
+# Exercício 2
+@app.route('/versao')
+def versao():
+    versao = "1.0.0"
+    return f"App v{versao}"
